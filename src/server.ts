f169eb42fd6fea278a06ba306a1fc0d7ed73b051
mohasinakonda <mohasinakonda39@gmail.com>
2023-05-29 15:40:24 +0600

    feat: initial setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+console.log(config.db_url);
+
+async function main() {
+	try {
+		await mongoose.connect(config.db_url as string);
+		console.log("db connected!");
+	} catch (err) {
+		console.log("failed to db connect", err);
+	}
+}
+main();
+
+app.listen(config.port, () => {
+	console.log(`server is running on port ${config.port}`);
+});
