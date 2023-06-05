144187ca192253b3cad31b0672357e99f64f54ba
mohasinakonda <mohasinakonda39@gmail.com>
2023-06-05 21:59:27 +0600

    feat: global config file for env

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
-	port: process.env.PORT,
-	db_url: process.env.DB_URL,
+    port: process.env.PORT,
+    db_url: process.env.DB_URL,
+    student_default_pass: process.env.STUDENT_DEFAULT_PASS,
 };
