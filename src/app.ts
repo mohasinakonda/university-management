380fcbd31c7c5ac9070066502527aeb9a76cec24
mohasinakonda <mohasinakonda39@gmail.com>
2023-06-05 22:00:03 +0600

    chore: update server and app file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-import express from "express";
+import express, { urlencoded, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
+import userRoute from "./modules/users/users.routes";
+import logger from "./shared/logger/logger";
 const app = express();
+dotenv.config();
+//middleware
 app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+//routes
+app.use("/api/v1", userRoute);
+
+app.get("/", async (req: Request, res: Response) => {
+    logger.info("server running successfully!");
+    res.send("working successfully!");
+});
+
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config/index";
-console.log(config.db_url);
+import logger from "./shared/logger/logger";
 
 async function main() {
-	try {
-		await mongoose.connect(config.db_url as string);
-		app.listen(config.port, () => {
-			console.log(`server is running on port ${config.port}`);
-		});
-		console.log("db connected!");
-	} catch (err) {
-		console.log("failed to db connect", err);
-	}
+    try {
+        await mongoose.connect(config.db_url as string);
+        app.listen(config.port, () => {
+            logger.info(`server is running on port ${config.port}`);
+        });
+        logger.info("db connected!");
+    } catch (err) {
+        logger.error("failed to db connect", err);
+    }
 }
 
 main();
