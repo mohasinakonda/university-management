3876a34cfc2d69cd3b0042c1870ffecfd0660db4
mohasinakonda <mohasinakonda39@gmail.com>
2023-06-18 12:20:14 +0600

    feat: get response

---
+++ src/shared/getResponse.ts
@@ -0,0 +1,23 @@
+import { Response } from "express";
+
+type IGetData<T> = {
+    status: boolean;
+    statusCode: number;
+    message?: string | null;
+    meta: {
+        page: number;
+        limit: number;
+        total: number;
+    };
+    data?: T | null;
+};
+export const getResponse = <T>(res: Response, data: IGetData<T>): void => {
+    const response: IGetData<T> = {
+        status: data.status,
+        statusCode: data.statusCode,
+        message: data.message,
+        meta: data.meta,
+        data: data.data,
+    };
+    res.status(data.statusCode).json(response);
+};
