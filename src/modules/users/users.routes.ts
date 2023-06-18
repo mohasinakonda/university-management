1af3ca943444cf6432f96c56c41ed791b437c41e
mohasinakonda <mohasinakonda39@gmail.com>
2023-06-18 12:19:32 +0600

    refactor : organize routes

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -2,6 +2,6 @@ import express from "express";
 import create_user from "./user.controller";
 const router = express.Router();
 
-router.post("/create-user", create_user);
+router.post("/", create_user);
 
-export default router;
+export const userRoute = router;
